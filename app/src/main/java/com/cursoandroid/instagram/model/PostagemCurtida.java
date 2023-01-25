d932878ff938b4278b725e8ed5c84e37bee2e68d
José Nobre <jnobrecf@gmail.com>
2023-01-25 15:41:40 -0300

    first Commit

---
+++ app/src/main/java/com/cursoandroid/instagram/model/PostagemCurtida.java
@@ -0,0 +1,72 @@
+package com.cursoandroid.instagram.model;
+
+import com.cursoandroid.instagram.helper.ConfiguracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.HashMap;
+
+public class PostagemCurtida {
+
+    public Feed feed;
+    public Usuario usuario;
+    public int qtdCurtidas = 0;
+
+    public PostagemCurtida() {
+    }
+
+    public void salvar(){
+
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebase();
+
+        //Objeto usuario
+        HashMap<String, Object> dadosUsuario = new HashMap<>();
+        dadosUsuario.put("nomeUsuario", usuario.getNome());
+        dadosUsuario.put("caminhoFoto", usuario.getCaminhoFoto());
+
+        DatabaseReference pCurtidasRef = firebaseRef
+                .child("postagens-curtidas")
+                .child(feed.getId())//id_postagem
+                .child(usuario.getId());//id_usuario_logado
+        pCurtidasRef.setValue(dadosUsuario);
+
+        //Atualizar quantidade de curtidas
+        atualizarQtd(1);
+    }
+
+    public void atualizarQtd(int valor){
+
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebase();
+
+        DatabaseReference pCurtidasRef = firebaseRef
+                .child("postagens-curtidas")
+                .child(feed.getId())//id_postagem
+                .child("qtdCurtidas");
+        setQtdCurtidas(getQtdCurtidas() + valor);
+        pCurtidasRef.setValue(getQtdCurtidas());
+
+    }
+
+    public Feed getFeed() {
+        return feed;
+    }
+
+    public void setFeed(Feed feed) {
+        this.feed = feed;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getQtdCurtidas() {
+        return qtdCurtidas;
+    }
+
+    public void setQtdCurtidas(int qtdCurtidas) {
+        this.qtdCurtidas = qtdCurtidas;
+    }
+}
